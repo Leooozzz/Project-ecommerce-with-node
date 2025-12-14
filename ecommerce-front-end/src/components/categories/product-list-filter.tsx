f2063aa784693cc6c8a10c5b33c9b1a9abe308bd
Leooozzz <leozeraffcanal2405@gmail.com>
2025-12-14 00:45:54 -0300

    making sidebar for items

--- ecommerce-front-end/src/components/categories/product-list-filter.tsx
+++ ecommerce-front-end/src/components/categories/product-list-filter.tsx
@@ -1,17 +1,24 @@
-"use client"
+"use client";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectValue,
+  SelectTrigger,
 } from "@/components/ui/select";
-import { SelectIcon, SelectTrigger } from "@radix-ui/react-select";
-import { ChevronDown } from "lucide-react";
-import { useState } from "react";
+import { useQueryString } from "@/hooks/use-query-string";
+import {useState } from "react";
+import { FilterGroup } from "./filter-group";
+
 export const ProductListFilter = () => {
+  const queryString = useQueryString();
+  const [filterOpened, setFilterOpened] = useState(false);
+  const order = queryString.get("order") ?? "views";
 
-    const [filterOpened,setFilterOpened]=useState(false)
 
+  const handleSelectChange=(value:string)=>{
+      queryString.set('order',value)
+  }
   return (
     <div>
       <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-6">
@@ -20,22 +27,37 @@ export const ProductListFilter = () => {
         </div>
         <div className="w-full flex flex-row md:max-w-70 gap-5">
           <div className="flex-1">
-            <Select>
-              <SelectTrigger className="w-[180px] md:w-full px-6 h-14 flex  items-center bg-black text-white border-gray-200 rounded-sm justify-between">
-                <SelectValue placeholder="Ordenar por" />
-                <SelectIcon>
-                  <ChevronDown className="h-5 w-5 text-white" />
-                </SelectIcon>
+            <Select
+              defaultValue={order}
+              onValueChange={handleSelectChange}
+            >
+              <SelectTrigger className="w-[180px]  md:w-full p-7  flex  items-center bg-black text-white border-gray-200 rounded-sm justify-between">
+                <SelectValue placeholder="Ordenar por" className="text-white" />
               </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="views">Popularidade</SelectItem>
+                <SelectItem value="price">Por preço</SelectItem>
+                <SelectItem value="selling">Mais pedidos</SelectItem>
+              </SelectContent>
             </Select>
           </div>
-          <div onClick={()=>setFilterOpened(!filterOpened)} className="w-[180px] md:hidden px-6 h-14 flex  items-center bg-black text-white border-gray-200 rounded-sm">
+          <div
+            onClick={() => setFilterOpened(!filterOpened)}
+            className="w-[180px] md:hidden p-6 h-14 flex  items-center bg-black text-white border-gray-200 rounded-sm"
+          >
             Filtrar por
           </div>
         </div>
       </div>
       <div className="flex flex-col md:flex-row gap-8 mt-8">
-        <div className={`flex-1 md:max-w-70 bg-red-200 ${filterOpened ? 'block' : 'hidden'} md:block`}>Filtros</div>
+        <div
+          className={`flex-1 md:max-w-70  ${
+            filterOpened ? "block" : "hidden"
+          } md:block`}
+        >
+         <FilterGroup/>
+          <FilterGroup/>
+        </div>
         <div className="flex-1 grid grid-cols-1 md:grid-cols-3">
           <div>...</div>
           <div>...</div>
